ac0c04c13c19f665134d9af49ba16c22016767ff
ujjawal <ujjawal17032002@gmail.com>
2024-12-13 01:33:30 +0530

    base model book2

--- books2.py
+++ books2.py
@@ -1,6 +1,19 @@
+from uuid import UUID
+
+from pydantic import BaseModel
+
 from fastapi import FastAPI
 
 app = FastAPI()
+
+
+class Book(BaseModel):
+    id: UUID
+    title: str
+    author: str
+    description: str
+    rating: int
+
 
 BOOKS = []
 
