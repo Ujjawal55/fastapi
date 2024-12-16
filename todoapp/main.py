8e984c7891a9161e14a9fe1724043534f40a2b54
ujjawal <ujjawal17032002@gmail.com>
2024-12-17 02:57:20 +0530

    post request

--- todoapp/main.py
+++ todoapp/main.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 from fastapi import Depends, FastAPI, HTTPException
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 
 import models
@@ -17,9 +20,37 @@ def get_db():
         db.close()
 
 
+# pydjantic model for the post request::
+
+
+class TodoCreate(BaseModel):
+    title: str
+    description: Optional[str]
+    priority: int = Field(
+        ge=1, le=5, description="priority should be in between 1 and 5"
+    )
+    complete: bool
+
+
 @app.get("/")
 async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Todo).all()
+
+
+# post request to create the todo
+@app.post("/")
+async def create_todo(todo: TodoCreate, db: Session = Depends(get_db)):
+    db_todo = models.Todo(
+        title=todo.title,
+        description=todo.description,
+        priority=todo.priority,
+        complete=todo.complete,
+    )
+
+    db.add(db_todo)
+    db.commit()
+
+    return {"status_code": 201, "message": "record has been successfully created"}
 
 
 @app.get("/todo/{todo_id}")
