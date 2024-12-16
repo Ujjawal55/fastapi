a615d2e9772b7e6a80c35b93022ae38417bcdc2e
ujjawal <ujjawal17032002@gmail.com>
2024-12-17 03:53:30 +0530

    put and delete request

--- todoapp/main.py
+++ todoapp/main.py
@@ -62,5 +62,42 @@ async def read_todo(todo_id: int, db: Session = Depends(get_db)):
     raise http_exception_404()
 
 
+# put request to the todo
+@app.put("/{todo_id}")
+async def update_todo(todo_id: int, todo: TodoCreate, db: Session = Depends(get_db)):
+    # get the particular instance of the data
+    todo_model = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+
+    if todo_model is None:
+        raise http_exception_404()
+
+    todo_model.title = todo.title  # type: ignore
+    todo_model.description = todo.description  # type: ignore
+    todo_model.priority = todo.priority  # type: ignore
+    todo_model.complete = todo.complete  # type: ignore
+
+    db.add(todo_model)
+    db.commit()
+
+    return http_status_code_200()
+
+
+# delete request
+@app.delete("/{todo_id}")
+async def delete_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+
+    if todo_model is None:
+        raise http_exception_404()
+
+    db.delete(todo_model)
+    db.commit()
+    return http_status_code_200()
+
+
+def http_status_code_200():
+    return {"status": 200, "message": "successfull"}
+
+
 def http_exception_404():
     return HTTPException(status_code=404, detail="Todo does not exist.")
