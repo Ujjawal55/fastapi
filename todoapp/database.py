e3be5fb17c54ceb0acc351dd877f50dd7255afc6
ujjawal <ujjawal17032002@gmail.com>
2024-12-22 17:33:09 +0530

    connect to postgres and test

--- todoapp/database.py
+++ todoapp/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQL_DATABASE_URL = "sqlite:///./todos.db"
+SQL_DATABASE_URL = "postgresql://postgres:pan123@localhost/TodoApplicationDatabase"
 
-engine = create_engine(SQL_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQL_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
